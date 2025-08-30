2fa60e9f8f501680a0de025313300e34f65ef339
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-30 19:43:53 +0530

    Created Cunstructor Function

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 /**
- * Constructor Function in JavaScript
+ * Constructor Function
  */
+function IndoBank(accountHolderName, contact, bankBalance = 0) {
+    this.accountHolderName = accountHolderName;
+    this.accountNumber = Date.now();
+    this.bankBalance = bankBalance;
+    this.contact = contact;
+}
+
+const rani = new IndoBank("Rani Yadav", 9876543211, 1000);
+const raja = new IndoBank("Raja Pal", 1234567899);
+
+console.log(rani, raja);
